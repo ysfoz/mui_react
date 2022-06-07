081a49915529c78a2d9ab3c52ffd05f7c889d7d7
ysfoz <ysfozturk3@gmail.com>
2022-06-07 22:48:49 +0200

    sidebar completed

---
+++ src/components/ListItemComponent.jsx
@@ -0,0 +1,24 @@
+import { Home } from "@mui/icons-material";
+import {
+  List,
+  ListItem,
+  ListItemText,
+  ListItemButton,
+  ListItemIcon,
+  Switch,
+} from "@mui/material";
+
+const ListItemComponent = (props) => {
+  return (
+    <List>
+      <ListItem disablePadding>
+        <ListItemButton component="a" href="#simple-list">
+          <ListItemIcon>{props.icon}</ListItemIcon>
+          {props.switch ? <Switch/> : <ListItemText primary={props.primary} />}
+        </ListItemButton>
+      </ListItem>
+    </List>
+  );
+};
+
+export default ListItemComponent;
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,18 @@
 import { Box } from "@mui/material";
+import ListItemComponent from "./ListItemComponent";
+import { AccountCircle, Article, Groups, Home, NightlightRound, Person, Settings, Shop } from "@mui/icons-material";
 
 const Sidebar = () => {
   return (
-    <Box bgcolor={"crimson"} flex={1} p={3} sx={{display:{xs:"none",sm:"block"}}}>
-      Sidebar
+    <Box flex={1} p={3} sx={{display:{xs:"none",sm:"block"}}}>
+      <ListItemComponent primary="Home" icon={<Home/>}/>
+      <ListItemComponent primary="Pages" icon={<Article/>}/>
+      <ListItemComponent primary="Groups" icon={<Groups/>}/>
+      <ListItemComponent primary="Marketplace" icon={<Shop/>}/>
+      <ListItemComponent primary="Friends" icon={<Person/>}/>
+      <ListItemComponent primary="Settings" icon={<Settings/>}/>
+      <ListItemComponent primary="Profile" icon={<AccountCircle/>}/>
+      <ListItemComponent primary="Home" icon={<NightlightRound/>} switch/>
     </Box>
   );
 };
